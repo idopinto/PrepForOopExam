d7c217cd2daa209b250bb00ae34f55bc6b51681f
wolfo1 <70093754+wolfo1@users.noreply.github.com>
2022-01-15 19:59:10 +0200

    Update Solutions.java

--- Exam Algorithms/Solutions.java
+++ Exam Algorithms/Solutions.java
@@ -17,7 +17,7 @@ public class Solutions {
                 if (i == 0 && j != 0)
                     grid[i][j] += grid[i][j - 1];
                 else if (i != 0 && j == 0)
-                    grid[i][j] += grid[i - 1][0];
+                    grid[i][j] += grid[i - 1][j];
                 else if (i != 0 && j != 0)
                     grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
             }
